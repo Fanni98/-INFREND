97df9f7d980d42350f57b604cf50885c1b5a1552
Fanni98 <36487318+Fanni98@users.noreply.github.com>
2020-05-15 23:56:53 +0200

    Add files via upload

--- src/java/videoteka/Dvd.java
+++ src/java/videoteka/Dvd.java
@@ -6,126 +6,192 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.context.FacesContext; 
 
-@ManagedBean  
-@RequestScoped  
+
+@ManagedBean
+@RequestScoped
 public class Dvd {
-   
-	private int sorszam;
-	private String cim;
-	private String beszerzesDatum;
-	private String statusz;
-        private ArrayList dvdLista ;  
-        private ArrayList cimsorszam ;
-        private Map<String,Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();  
-        Connection connection; 
-        
-	public String getCim() {
-		return cim;
-	}
-
-	public void setCim(String cim) {
-		this.cim = cim;
-	}
-
-	public String getBeszerzesDatum() {
-		return beszerzesDatum;
-	}
-
-	public void setBeszerzesDatum(String beszerzesDatum) {
-		this.beszerzesDatum = beszerzesDatum;
-	}
-
-	public int getSorszam() {
-		return sorszam;
-	}
-
-	public void setSorszam(int sorszam) {
-		this.sorszam = sorszam;
-	}
-
-	public String getStatusz() {
-		return statusz;
-	}
-
-	public void setStatusz(String statusz) {
-		this.statusz = statusz;
-	}
-        public Connection getConnection(){  
-            try{  
-                Class.forName("com.mysql.jdbc.Driver");     
-                connection = DriverManager.getConnection( "jdbc:mysql://localhost:3306/videoteka?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","root");  
-            }
-            catch(Exception e){  
-                System.out.println(e);  
-            }  
-            return connection;
-         }
-        public ArrayList dvdLista(){  
-         try{  
-             dvdLista = new ArrayList();  
-             connection = getConnection();  
-             Statement stmt=getConnection().createStatement();    
-             ResultSet rs=stmt.executeQuery("select * from dvdkazetta ");    
-            while(rs.next()){  
-                Dvd dvd = new Dvd();   
-                dvd.setSorszam(rs.getInt("sorszam"));  
-                dvd.setCim(rs.getString("cim"));  
-                dvd.setBeszerzesDatum(rs.getString("beszerzesdatum"));   
-                dvd.setStatusz(rs.getString("statusz"));  
-                dvdLista.add(dvd);  
-            }  
-            connection.close();          
-            }catch(Exception e){  
-                System.out.println(e);  
-            }  
-                return dvdLista;  
-            } 
-        public ArrayList cimSorszam(){  
-         try{  
-             cimsorszam = new ArrayList();  
-             connection = getConnection();  
-             Statement stmt=getConnection().createStatement();    
-             ResultSet rs=stmt.executeQuery("select * from dvdkazetta where statusz='kikölcsönzött'");    
-            while(rs.next()){  
-                Dvd dvd = new Dvd();   
-                dvd.setSorszam(rs.getInt("sorszam"));  
-                dvd.setCim(rs.getString("cim"));  
-              
-                cimsorszam.add(dvd);  
-            }  
-            connection.close();          
-            }catch(Exception e){  
-                System.out.println(e);  
-            }  
-                return cimsorszam;  
-            }  
-        public void statuszvaltasKolcsonzes(int sorszam){  
-        try{  
-            connection = getConnection();
-            PreparedStatement stmt = connection.prepareStatement("update dvd set statusz = 'kikölcsönzött' where sorszam = ?");
-            stmt.setInt(1, sorszam);
-            stmt.execute();    
-            }catch(Exception e){  
-            System.out.println(e);  
-            } }
-        
-        public void statuszvaltasSelejtezes(int sorszam){  
-        try{  
-            connection = getConnection();
-            PreparedStatement stmt = connection.prepareStatement("update dvd set statusz = 'selejtezett' where sorszam = ?");
-            stmt.setInt(1, sorszam);
-            stmt.execute();    
-            }catch(Exception e){  
-            System.out.println(e);  
-            } 
-        
-        }
-        }
     
+    private int sorszam;
+    private String cim;
+    private String beszerzesDatum;
+    private String statusz;
+    private ArrayList dvdLista;
+    private ArrayList cimsorszam;
+    private ArrayList kolcsonozhetoDvd;
+    private ArrayList keresSorszam;
+    private ArrayList talalatsorszam;
+    Connection connection;
+    
+    public String getCim() {
+        return cim;
+    }
+    
+    public void setCim(String cim) {
+        this.cim = cim;
+    }
+    
+    public String getBeszerzesDatum() {
+        return beszerzesDatum;
+    }
+    
+    public void setBeszerzesDatum(String beszerzesDatum) {
+        this.beszerzesDatum = beszerzesDatum;
+    }
+    
+    public int getSorszam() {
+        return sorszam;
+    }
+    
+    public void setSorszam(int sorszam) {
+        this.sorszam = sorszam;
+    }
+    
+    public String getStatusz() {
+        return statusz;
+    }
+    
+    public void setStatusz(String statusz) {
+        this.statusz = statusz;
+    }
+    public Connection getConnection(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection( "jdbc:mysql://localhost:3306/videoteka?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","root");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return connection;
+    }
+    public ArrayList dvdLista(){
+        try{
+            dvdLista = new ArrayList();
+            connection = getConnection();
+            Statement stmt=getConnection().createStatement();
+            ResultSet rs=stmt.executeQuery("select * from dvdkazetta ");
+            while(rs.next()){
+                Dvd dvd = new Dvd();
+                dvd.setSorszam(rs.getInt("sorszam"));
+                dvd.setCim(rs.getString("cim"));
+                dvd.setBeszerzesDatum(rs.getString("beszerzesdatum"));
+                dvd.setStatusz(rs.getString("statusz"));
+                dvdLista.add(dvd);
+            }
+            connection.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return dvdLista;
+    }
+    public ArrayList keresSorszam(){
+        try{
+            keresSorszam = new ArrayList();
+            connection = getConnection();
+            Statement stmt=getConnection().createStatement();
+            ResultSet rs=stmt.executeQuery("SELECT * FROM dvdkazetta where sorszam like '"+  sorszam + "' ");
+            while(rs.next()){
+                Dvd dvd = new Dvd();
+                dvd.setSorszam(rs.getInt("sorszam"));
+                dvd.setCim(rs.getString("cim"));
+                dvd.setBeszerzesDatum(rs.getString("beszerzesdatum"));
+                dvd.setStatusz(rs.getString("statusz"));
+                keresSorszam.add(dvd);
+            }
+            connection.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return keresSorszam;
+    }
+    public ArrayList<Dvd> getValues(){
+        talalatsorszam = keresSorszam();
+        return talalatsorszam;
+        
+    }
+    public ArrayList kolcsonozhetoDvd(){
+        try{
+            kolcsonozhetoDvd = new ArrayList();
+            connection = getConnection();
+            Statement stmt=getConnection().createStatement();
+            ResultSet rs=stmt.executeQuery("select * from dvdkazetta where statusz='szabad'");
+            while(rs.next()){
+                Dvd dvd = new Dvd();
+                dvd.setSorszam(rs.getInt("sorszam"));
+                dvd.setCim(rs.getString("cim"));
+                dvd.setStatusz(rs.getString("statusz"));
+                kolcsonozhetoDvd.add(dvd);
+            }
+            connection.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return kolcsonozhetoDvd;
+    }
+    
+    public ArrayList cimSorszam(){
+        try{
+            cimsorszam = new ArrayList();
+            connection = getConnection();
+            Statement stmt=getConnection().createStatement();
+            ResultSet rs=stmt.executeQuery("select * from dvdkazetta where statusz='kikölcsönzött'");
+            while(rs.next()){
+                Dvd dvd = new Dvd();
+                dvd.setSorszam(rs.getInt("sorszam"));
+                dvd.setCim(rs.getString("cim"));
+                dvd.setStatusz(rs.getString("statusz"));
+                cimsorszam.add(dvd);
+            }
+            connection.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return cimsorszam;
+    }
+    
+    
+    
+    public void statuszSelejtezes(int sorszam){
+        try{
+            connection = getConnection();
+            PreparedStatement stmt = connection.prepareStatement("update dvdkazetta set statusz = 'selejtezett' where sorszam = ?");
+            stmt.setInt(1, sorszam);
+            stmt.execute();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+    
+    public void statuszSzabad(int sorszam){
+        try{
+            connection = getConnection();
+            PreparedStatement stmt = connection.prepareStatement("update dvdkazetta set statusz = 'szabad' where sorszam = ?");
+            stmt.setInt(1, sorszam);
+            stmt.execute();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+    
+    public void statuszKolcsonzes(int sorszam){
+        try{
+            connection = getConnection();
+            PreparedStatement stmt = connection.prepareStatement("update dvdkazetta set statusz='kikölcsönzött' where sorszam = ?");
+            stmt.setInt(1, sorszam);
+            stmt.execute();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+    
+    
+    
+}
+
     
 
--- src/java/videoteka/Kolcsonzes.java
+++ src/java/videoteka/Kolcsonzes.java
@@ -1,80 +1,107 @@
-package videoteka;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import javax.faces.context.FacesContext; 
-
+import java.sql.Connection;  
+import java.sql.DriverManager;  
+import java.sql.PreparedStatement;  
+import java.sql.Statement;  
+import java.sql.ResultSet;  
+import java.util.ArrayList;   
+import javax.faces.bean.ManagedBean;  
+import javax.faces.bean.RequestScoped;  
+ 
 @ManagedBean  
-@RequestScoped 
-public class Kolcsonzes {
-        private int id;
-	private int sorszam;
-	private String mikortol;
-        private ArrayList kolcsonzesLista ;  
-        private Map<String,Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();  
-        Connection connection; 
-	
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getSorszam() {
-		return sorszam;
-	}
-
-	public void setSorszam(int sorszam) {
-		this.sorszam = sorszam;
-	}
-
-	public String getMikortol() {
-		return mikortol;
-	}
-
-	public void setMikortol(String mikortol) {
-		this.mikortol = mikortol;
-	}
+@RequestScoped  
+public class Kolcsonzes{
+    private int id;
+    private int uid;
+    private int usorszam;
+    private String mikortol;
+    ArrayList kolcsonzesLista ;
+    Connection connection;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getUid() {
+        return uid;
+    }
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+    public int getUsorszam() {
+        return usorszam;
+    }
+    public void setUsorszam(int usorszam) {
+        this.usorszam = usorszam;
+    }
+    public String getMikortol() {
+        return mikortol;
+    }
+    public void setMikortol(String mikortol) {
+        this.mikortol = mikortol;
+    }  
         
-        public Connection getConnection(){  
-            try{  
-                Class.forName("com.mysql.jdbc.Driver");     
-                connection = DriverManager.getConnection( "jdbc:mysql://localhost:3306/videoteka?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","root");  
-            }
-            catch(Exception e){  
-                System.out.println(e);  
-            }  
-            return connection;
-         }
-        public ArrayList kolcsonzesLista(){  
-         try{  
-             kolcsonzesLista = new ArrayList();  
-             connection = getConnection();  
-             Statement stmt=getConnection().createStatement();    
-             ResultSet rs=stmt.executeQuery("select * from kolcsonzes order by uid");    
-            while(rs.next()){  
+    public Connection getConnection(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection( "jdbc:mysql://localhost:3306/videoteka?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","root");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return connection;
+    }
+    
+    public ArrayList kolcsonzesLista(){
+        try{
+            kolcsonzesLista = new ArrayList();
+            connection = getConnection();
+            Statement stmt=getConnection().createStatement();
+            ResultSet rs=stmt.executeQuery("select * from kolcsonzes order by uid");
+            while(rs.next()){
                 Kolcsonzes k = new Kolcsonzes();
-                k.setId(rs.getInt("uid"));
-                k.setSorszam(rs.getInt("usorszam"));  
-                k.setMikortol(rs.getString("mikortol"));  
-                kolcsonzesLista.add(k);  
-            }  
-            connection.close();          
-            }catch(Exception e){  
-                System.out.println(e);  
-            }  
-                return kolcsonzesLista;  
-            }  
+                k.setUid(rs.getInt("uid"));
+                k.setUsorszam(rs.getInt("usorszam"));
+                k.setMikortol(rs.getString("mikortol"));
+                kolcsonzesLista.add(k);
+            }
+            connection.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return kolcsonzesLista;
+    }
     
-   
+    public String save(){
+        int result = 0;
+        try{
+            connection = getConnection();
+            PreparedStatement stmt = connection.prepareStatement(
+                    "insert into kolcsonzes(uid,usorszam,mikortol) values(?,?,?)");
+            stmt.setInt(1, uid);
+            stmt.setInt(2, usorszam);
+            stmt.setString(3, mikortol);
+            result = stmt.executeUpdate();
+            connection.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        if(result !=0)
+            return "dvdfoglalasa.xhtml?faces-redirect=true";
+        else return "ujkolcsonzes.xhtml?faces-redirect=true";
+    }
     
-}
+    
+    public void delete(int usorszam){
+        try{
+            connection = getConnection();
+            PreparedStatement stmt = connection.prepareStatement("delete from kolcsonzes where usorszam = ?");
+            stmt.setInt(1, usorszam);
+            stmt.executeUpdate();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }  
+
+} 
+
